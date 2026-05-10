19b86c8866c9099dd3c9e22b7790e8f7c013da1c
CookerCandle <jamoliddingafurov3223@gmail.com>
2026-05-10 22:26:00 +0500

    dev page

---
+++ src/pages/DevPage.jsx
@@ -0,0 +1,11 @@
+// import { componentName } from ../components/ComponentName
+import { Navbar } from "../components/Navbar";
+const DevPage = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default DevPage;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 // pages
 import HomePage from "../pages/HomePage";
 import IndexHelper from "../pages/HelperPage";
+import DevPage from "../pages/DevPage";
 
 const Approuter = () => {
     return (
         <Routes>
             <Route index element={<HomePage />} />
+            <Route path="/dev" element={<DevPage />} />
             <Route path="/helper" element={<IndexHelper />} />
         </Routes>
     )
